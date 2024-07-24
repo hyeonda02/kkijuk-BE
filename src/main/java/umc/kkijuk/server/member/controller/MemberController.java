f8d6eb48422dca8f9e7d1e422298833a12e20140
Suhun0331 <rlatngns6046@naver.com>
2024-07-24 14:36:53 +0900

    Feat : 회원가입 기능 및 성공/실패 테스트케이스 추가

--- src/main/java/umc/kkijuk/server/member/controller/MemberController.java
+++ src/main/java/umc/kkijuk/server/member/controller/MemberController.java
@@ -1,13 +1,62 @@
 package umc.kkijuk.server.member.controller;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import umc.kkijuk.server.member.dto.MemberJoinDto;
 import umc.kkijuk.server.member.service.MemberService;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
 
-@Controller
+
+@Tag(name = "member", description = "회원 관리 API")
+@RestController
+@RequestMapping("/member")
 @RequiredArgsConstructor
 public class MemberController {
 
     private final MemberService memberService;
 
+    @Operation(
+            summary = "회원가입 요청",
+            description = "회원가입 요청을 받아 성공/실패 여부를 반환합니다.")
+    @PostMapping
+    public ResponseEntity<CreateMemberResponse> saveMember(@RequestBody @Valid MemberJoinDto memberJoinDto) {
+        String passwordConfirm = memberJoinDto.getPasswordConfirm();
+        if (!passwordConfirm.equals(memberJoinDto.getPassword())) {
+            return ResponseEntity
+                    .status(HttpStatus.BAD_REQUEST)
+                    .body(new CreateMemberResponse("Passwords do not match"));
+        }
+
+        try {
+            Long memberId = memberService.join(memberJoinDto.toEntity());
+            return ResponseEntity
+                    .status(HttpStatus.CREATED)
+                    .body(new CreateMemberResponse(memberId, "Member created successfully"));
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity
+                    .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(new CreateMemberResponse("Member creation failed"));
+        }
+    }
+
+    @Data
+    static class CreateMemberResponse {
+        private Long id;
+        private String message;
+
+        public CreateMemberResponse(String message) {
+            this.message = message;
+        }
+
+        public CreateMemberResponse(Long id, String message) {
+            this.id = id;
+            this.message = message;
+        }
+    }
 }
--- src/main/java/umc/kkijuk/server/member/domain/Member.java
+++ src/main/java/umc/kkijuk/server/member/domain/Member.java
@@ -16,7 +16,7 @@ import java.util.ArrayList;
 @AllArgsConstructor
 public class Member {
 
-    @Id @GeneratedValue
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "member_id")
     private Long id;
 
