c0a0f3e15a53c19c2e2bdaf9931f95d1e39de398
nanna29 <luna2907@naver.com>
2024-08-07 01:09:43 +0900

    feat: Fix MasterIntroduce Entity field

--- src/test/java/umc/kkijuk/server/record/service/RecordServiceTest.java
+++ src/test/java/umc/kkijuk/server/record/service/RecordServiceTest.java
@@ -1,30 +1,23 @@
 package umc.kkijuk.server.record.service;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.web.servlet.MockMvc;
-import umc.kkijuk.server.introduce.domain.IntroduceRepository;
 import umc.kkijuk.server.introduce.domain.QuestionRepository;
-import umc.kkijuk.server.introduce.service.IntroduceService;
 import umc.kkijuk.server.member.domain.MarketingAgree;
 import umc.kkijuk.server.member.domain.Member;
 import umc.kkijuk.server.member.domain.State;
 import umc.kkijuk.server.member.dto.MemberJoinDto;
 import umc.kkijuk.server.member.service.MemberService;
 import umc.kkijuk.server.record.domain.RecordRepository;
-import umc.kkijuk.server.recruit.domain.Recruit;
-import umc.kkijuk.server.recruit.service.port.RecruitRepository;
 
+import java.lang.Record;
 import java.time.LocalDate;
 
-import static org.junit.jupiter.api.Assertions.*;
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
@@ -39,6 +32,7 @@ class RecordServiceTest {
     @Autowired
     private MemberService memberService;
     private Member requestMember;
+    private Record requestRecord;
 
     @BeforeEach
     public void Init() {
