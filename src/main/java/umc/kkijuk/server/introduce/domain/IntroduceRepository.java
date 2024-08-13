2e6488698ec86434dec5d1389c8e4c060d260864
nanna29 <luna2907@naver.com>
2024-08-13 23:26:59 +0900

    repository

--- src/main/java/umc/kkijuk/server/introduce/domain/IntroduceRepository.java
+++ src/main/java/umc/kkijuk/server/introduce/domain/IntroduceRepository.java
@@ -1,9 +1,6 @@
 package umc.kkijuk.server.introduce.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-import umc.kkijuk.server.member.domain.Member;
 
 import java.util.List;
 import java.util.Optional;
