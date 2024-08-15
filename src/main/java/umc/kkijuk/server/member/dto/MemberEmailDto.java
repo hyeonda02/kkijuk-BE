eae55bb9f04ba2277a2870c130747da323a39140
Suhun0331 <rlatngns6046@naver.com>
2024-08-15 14:42:33 +0900

    Refactor : Email 형식 검사 어노테이션 추가

--- src/main/java/umc/kkijuk/server/member/dto/MemberEmailDto.java
+++ src/main/java/umc/kkijuk/server/member/dto/MemberEmailDto.java
@@ -1,5 +1,6 @@
 package umc.kkijuk.server.member.dto;
 
+import jakarta.validation.constraints.Email;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,6 +8,7 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 public class MemberEmailDto {
+    @Email
     private String email;
 
     @Builder
