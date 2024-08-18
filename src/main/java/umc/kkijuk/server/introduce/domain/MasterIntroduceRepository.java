b61aaaa2ad07ecb8424679a5f6e01eefa6d00fb4
nanna29 <luna2907@naver.com>
2024-08-19 02:35:20 +0900

    fix: MasterIntro constraints

--- src/main/java/umc/kkijuk/server/introduce/domain/MasterIntroduceRepository.java
+++ src/main/java/umc/kkijuk/server/introduce/domain/MasterIntroduceRepository.java
@@ -7,4 +7,5 @@ import java.util.Optional;
 
 public interface MasterIntroduceRepository extends JpaRepository<MasterIntroduce, Long> {
     MasterIntroduce findByMemberId(Long memberId);
+    boolean existsByMemberId(Long memberId);
 }
