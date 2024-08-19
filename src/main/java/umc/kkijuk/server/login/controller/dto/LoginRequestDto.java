bd077041b4b9ea80e612010ccdac465a9367c274
JeongHoon Lee <sherry0_@kakao.com>
2024-08-19 22:04:19 +0900

    fix: add allow-Credential header

--- src/main/java/umc/kkijuk/server/login/controller/dto/LoginRequestDto.java
+++ src/main/java/umc/kkijuk/server/login/controller/dto/LoginRequestDto.java
@@ -1,5 +1,6 @@
 package umc.kkijuk.server.login.controller.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
@@ -8,6 +9,7 @@ import lombok.Getter;
 public class LoginRequestDto {
     @NotBlank(message = "이메일은 필수사항입니다.")
     @Email(message = "이메일 형식이 잘못되었습니다.")
+    @Schema(description = "이메일", example = "test@gmail.com", type = "string")
     private String email;
     @NotBlank(message = "비밀번호는 필수사항입니다.")
     private String password;
