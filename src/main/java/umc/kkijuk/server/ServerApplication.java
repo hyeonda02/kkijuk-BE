0a853fd8170c9c0683555abcb4871f7ddabf2019
JeongHoon Lee <sherry0_@kakao.com>
2024-08-21 22:53:36 +0900

    fix: timezone setting

--- src/main/java/umc/kkijuk/server/ServerApplication.java
+++ src/main/java/umc/kkijuk/server/ServerApplication.java
@@ -1,12 +1,21 @@
 package umc.kkijuk.server;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 @EnableJpaAuditing
 public class ServerApplication {
+
+
+	@PostConstruct
+	public void started() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServerApplication.class, args);
